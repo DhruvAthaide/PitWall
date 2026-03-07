24e8c4c32d3251ab3855cf1acf71d831e53f853c
Dhruv Athaide <athaidedhruv@gmail.com>
2026-03-07 10:23:53 +0530

    Updated Project Structure

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\athai\VSCode\Personal Coding\React Projects\F1FantasyPrediction\backend\venv
+set VIRTUAL_ENV=C:\Users\athai\VSCode\Personal Coding\React Projects\PitWall\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
